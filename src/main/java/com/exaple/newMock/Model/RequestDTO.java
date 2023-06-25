0564014ea8ecda5542b64e5817e3814139f025c9
MaxMartynov0727 <maxlion1994@gmail.com>
2023-06-25 22:33:14 +0500

    first commit

---
+++ src/main/java/com/exaple/newMock/Model/RequestDTO.java
@@ -0,0 +1,18 @@
+package com.exaple.newMock.Model;
+
+import lombok.*;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RequestDTO {
+
+    private String rqUID;
+    private String clientId;
+    private String account;
+    private String openDate;
+    private String closeDate;
+
+}
